e8f4118a647a003486dab53e52d5767cb755ce82
feniljain <fkjainco@gmail.com>
2026-07-31 22:12:20 +0530

    wip: nested loop join

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,10 @@
+// pub const MergedFieldsAndArrResult = struct {
+//     fields: []Field,
+//     arrs: [][],
+// };
+
 pub fn main() !void {
-    const smp_allocator: Allocator = .{
+    const alloc: Allocator = .{
         .ptr = undefined,
         .vtable = &SmpAllocator.vtable,
     };
@@ -7,18 +12,53 @@ pub fn main() !void {
     var prng = std.Random.DefaultPrng.init(0x1234_5678_9ABC_DEF0);
     // TODO(feniljian): make this generate 8192 rows and keep feeding in
     // to join algo
-    var result = try benchmark.genDataset(smp_allocator, prng.random());
+    var result = try benchmark.genDataset(alloc, prng.random());
 
     std.debug.print("Size of build record batch: {}\n", .{result.build.numRows()});
 
+    const resultFields = try mergeSchemas(alloc, &benchmark.buildFields, &benchmark.probeFields, 0);
+    // std.debug.print("DEBUG::fields::{any}\n", .{fields});
+
+    const buildJoinCol = PrimitiveArray(u64){ .data = result.build.column(0).data() };
+
+    var validIndices = [_]usize{0} ** benchmark.DEFAULT_BATCH_SIZE;
+    var validIndicesIdx: usize = 0;
     while(try result.probeIter.next()) |probeBatch| {
         std.debug.print("Size of probe record batch: {}\n", .{probeBatch.numRows()});
+        const probeJoinCol = PrimitiveArray(u64){ .data = probeBatch.column(0).data() };
+
+        var resultBatchBuilder = try RecordBatchBuilder.initBorrowed(alloc, .{ .fields = resultFields });
+        defer resultBatchBuilder.deinit();
+
+        for(try buildJoinCol.values(), 0..buildJoinCol.len()) |buildVal, buildIdx| {
+            for(try probeJoinCol.values(), 0..probeJoinCol.len()) |probeVal, probeIdx| {
+                if(std.meta.eql(buildVal, probeVal)) {
+                    validIndices[validIndicesIdx] = probeIdx;
+                    validIndicesIdx += 1;
+                }
+            }
+
+            for(0..result.build.numColumns()) |colIdx| {
+                var arr = try UInt64Builder.init(alloc, benchmark.DEFAULT_BATCH_SIZE);
+                const col = PrimitiveArray(u64){ .data = result.build.column(colIdx).data() };
+                const val = try col.value(buildIdx);
+                for(0..benchmark.DEFAULT_BATCH_SIZE) |_| {
+                    try arr.append(val);
+                }
+
+                try resultBatchBuilder.setColumn(colIdx, try arr.finish());
+            }
+
+            var resultColIdx = result.build.numColumns();
+            for(0..probeBatch.numColumns()) |colIdx| {
+                const col = probeBatch.column(colIdx);
+                const datum = ComputeDatum.fromArray(*col);
+                const filteredDatum = try computeDatumTake(datum, validIndices);
+                try resultBatchBuilder.setColumn(resultColIdx, filteredDatum.asArray() orelse return null);
+                resultColIdx += 1;
+            }
+        }
     }
-
-    const fields = try mergeSchemas(smp_allocator, &benchmark.buildFields, &benchmark.probeFields, 0);
-    std.debug.print("DEBUG::fields::{any}\n", .{fields});
-
-    // nestedLoopJoin(result, 0, 0);
 }
 
 fn mergeSchemas(alloc: Allocator, buildFields: []const Field, probeFields: []const Field, probeJoinColIdx: u32) ![]Field {
@@ -39,50 +79,26 @@ fn mergeSchemas(alloc: Allocator, buildFields: []const Field, probeFields: []con
     return resultFields;
 }
 
-// // TOOD(feniljain):
-// // - X implement merge schema
-// // - X convert probe side generation to an iterator
-// // - loop over iterator, then loop over the received batch, then loop over the build side
-// // - accumulate results into new batch with new schema and for start get answers using value(idx) directly
-// // - then optimize this loop
+// TOOD(feniljain):
+// - X implement merge schema
+// - X convert probe side generation to an iterator
+// - loop over iterator, then loop over the received batch, then loop over the build side
+// - accumulate results into new batch with new schema and for start get answers using value(idx) directly
+// - then optimize this loop
+
+// Inner Join Nested Loop Join
 //
-// // Inner Join Nested Loop Join
-// //
-// // Datafusion:
-// //
-// // points:
-// // - all build side batches are in memory
-// // - probe side is streamed until threshold is reached
-// //
-// // process:
-// // - create a bitmap of selected results
-// // - remove all the unwanted rows using filter bitmap
-// // - copy all the required columns into a new batch
-// // - return when you hit configured record batch size
-// fn nestedLoopJoin(result: GenDatasetResult, buildJoinColIdx: u32, probeJoinColIdx: u32) void {
-//     // loop over join column in build side
-//     // - init vector for this iteration
-//     // - loop over join column in probe side
-//     // - find matches and push to vector
+// Datafusion:
 //
-//     // research: codex resume 019fa7ff-7729-7803-ba4d-a6c297f877bf
+// points:
+// - all build side batches are in memory
+// - probe side is streamed until threshold is reached
 //
-//     const probeJoinCol = result.probe.column(probeJoinColIdx);
-//     const buildJoinCol = result.build.column(buildJoinColIdx);
-//     for(buildJoinCol.values(), 0..buildJoinCol.len) |buildVal, buildIdx| {
-//         const vec = ArrayList(u64);
-//         for(probeJoinCol.values(), 0..probeJoinCol.len) |probeVal, probeIdx| {
-//             if(std.meta.eql(buildVal, probeVal)) {
-//                 // construct row and push into vector
-//                 // vec.append();
-//             }
-//         }
-//     }
-//
-//     // convert row format to column?
-//
-//     // return record batches
-// }
+// process:
+// - create a bitmap of selected results
+// - remove all the unwanted rows using filter bitmap
+// - copy all the required columns into a new batch
+// - return when you hit configured record batch size
 
 const std = @import("std");
 const SmpAllocator = std.heap.SmpAllocator;
@@ -96,3 +112,10 @@ const zarrow = @import("zarrow");
 const RecordBatch = zarrow.RecordBatch;
 const Schema = zarrow.Schema;
 const Field = zarrow.Field;
+const MutableValidityBitmap = zarrow.MutableValidityBitmap;
+const ComputeDatum = zarrow.ComputeDatum;
+const computeDatumTake = zarrow.computeDatumTake;
+const PrimitiveBuilder = zarrow.PrimitiveBuilder;
+const RecordBatchBuilder = zarrow.RecordBatchBuilder;
+const PrimitiveArray = zarrow.PrimitiveArray;
+const UInt64Builder = zarrow.UInt64Builder;
