92487dd6c396f53e73963f761f72eb12db149419
feniljain <fkjainco@gmail.com>
2026-08-03 19:03:02 +0530

    feat: colaesce batches compiles

--- src/arrow_utils.zig
+++ src/arrow_utils.zig
@@ -29,15 +29,34 @@ pub fn buildRecordBatch(alloc: Allocator, arr: []u64, fields: []const Field) !Re
     return try recordBatchBuilder.finish();
 }
 
-pub const ConactRecordBatch = struct {
+pub fn mergeBatches(alloc: Allocator, first: RecordBatch, second: RecordBatch) !RecordBatch {
+    const schemaRef = try SchemaRef.fromBorrowed(alloc, (first.schema()).*);
+    var builder = try RecordBatchBuilder.initBorrowed(alloc, schemaRef);
+    defer builder.deinit();
+
+    for (0..first.numColumns()) |colIdx| {
+        const mergedArr = try concatArrayRefs(first.column(colIdx), second.column(colIdx));
+        builder.setColumn(colIdx, mergedArr);
+    }
+
+    return try builder.finish();
+}
+
+pub const CoalesceBatches = struct {
     targetBatchSize: u64,
     alloc: Allocator,
-    // resultQueue: ;
+    resultQueue: Deque,
+    incompleteBatchOpt: ?RecordBatch,
 
     const Self = @This();
 
     pub fn init(alloc: Allocator, targetBatchSize: u64) Self {
-        return .{ .alloc = alloc, .targetBatchSize = targetBatchSize };
+        return .{
+            .alloc = alloc,
+            .targetBatchSize = targetBatchSize,
+            .resultQueue = Deque.initCapacity(alloc, 10),
+            .incompleteBatchOpt = null,
+        };
     }
 
     // - if len is greater than 8192, break into
@@ -46,7 +65,39 @@ pub const ConactRecordBatch = struct {
     // - if len is less than 8192, merge with current stored record
     // till 8192 is reached and then push that to queue.
     // - And finally pop from queue when asked for result
-    pub fn registerRecordBatch(_: RecordBatch) void {
+    pub fn registerRecordBatch(self: *Self, recordBatch: RecordBatch) !void {
+        var batchTrackingIdx = 0;
+        const nRows = recordBatch.numRows();
+        while(nRows >= self.targetBatchSize) {
+            self.resultQueue.pushBack(try recordBatch.slice(batchTrackingIdx, self.targetBatchSize));
+            batchTrackingIdx += self.targetBatchSize;
+        }
+
+        // we have exhausted record batch
+        const remainingRows = nRows - batchTrackingIdx;
+        if(remainingRows == 0) {
+            return;
+        }
+
+        // if incomplete batch is not present, current record batch becomes the one
+        var neededRows = remainingRows;
+        if (self.incompleteBatchOpt) |incompleteBatch| {
+            neededRows = self.targetBatchSize - self.incompleteBatchOpt.numRows();
+            if(remainingRows <= neededRows) {
+                const batch = try mergeBatches(self.alloc, incompleteBatch, try recordBatch.slice(batchTrackingIdx, remainingRows));
+                if(remainingRows == neededRows) {
+                    self.resultQueue.pushBack(batch);
+                } else {
+                    self.incompleteBatchOpt = batch;
+                }
+            } else {
+                const batch = try mergeBatches(self.alloc, incompleteBatch, try recordBatch.slice(batchTrackingIdx, neededRows));
+                self.resultQueue.pushBack(batch);
+
+                batchTrackingIdx += neededRows;
+                self.incompleteBatchOpt = try recordBatch.slice(batchTrackingIdx, remainingRows - neededRows);
+            }
+        }
     }
 
     // pub fn next(self: *Self) !?RecordBatch {
@@ -56,8 +107,12 @@ pub const ConactRecordBatch = struct {
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
+const Deque = @import("deque");
+
 const zarrow = @import("zarrow");
 const Field = zarrow.Field;
 const RecordBatchBuilder = zarrow.RecordBatchBuilder;
 const RecordBatch = zarrow.RecordBatch;
 const ArrayRef = zarrow.ArrayRef;
+const concatArrayRefs = zarrow.concatArrayRefs;
+const SchemaRef = zarrow.SchemaRef;
